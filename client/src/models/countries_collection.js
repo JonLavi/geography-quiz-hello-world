0c20790561c56e3a37fdeb9d520545bc7bb6292e
Jonathan Lavi <jonathan.lavi@gmail.com>
2019-01-25 13:56:19 +0000

    add retrieveAllHellos()

--- client/src/models/countries_collection.js
+++ client/src/models/countries_collection.js
@@ -5,3 +5,31 @@
 
 // Path 2
 // Use dummy data to request to request all of those countries API objects
+
+const Request = require('../helpers_request_helper.js');
+
+
+const CountriesCollection = function () {
+  this.hellos = []
+}
+
+
+CountriesCollection.prototype.buildGameData = function () {
+  /// do path 1: this.retrieveAllHellos();
+  /// do path 2
+  /// combine
+};
+
+CountriesCollection.prototype.retrieveAllHellos = function () {
+  const hellosRequest = new Request('api/hellos')
+  request
+    .get()
+    .then((listItems) => {
+      this.hellos = listItems
+    }
+};
+
+
+
+
+module.exports = ContriesCollection;
