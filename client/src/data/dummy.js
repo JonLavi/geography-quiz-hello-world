9d30dec088aac8a152af4f788db4bc89cac0fc88
Rick Symington <rick.symington@gmail.com>
2019-01-25 13:37:25 +0000

    Add dummy data + paths

---
+++ client/src/data/dummy.js
@@ -0,0 +1,18 @@
+const sample_data =
+
+[{
+  country: "France",
+  hello: "bonjour"
+},
+{
+  country: "Spain",
+  hello: "hola"
+},
+{
+  country: "Germany",
+  hello: "guten tag"
+}
+]
+
+
+module.exports = sample_data
---
+++ client/src/models/countries_collection.js
@@ -0,0 +1,7 @@
+// Path 1
+// Datababase request to MongoDB
+// Return an array of country/hello objects
+// Save into local constant
+
+// Path 2
+// Use dummy data to request to request all of those countries API objects
