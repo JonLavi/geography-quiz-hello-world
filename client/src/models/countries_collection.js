8338bffa2e7062937bcc5e7d3cd3045a7386cca3
Rick Symington <rick.symington@gmail.com>
2019-01-25 13:55:55 +0000

    Add countries collection initial rq function

--- client/src/models/countries_collection.js
+++ client/src/models/countries_collection.js
@@ -5,3 +5,28 @@
 
 // Path 2
 // Use dummy data to request to request all of those countries API objects
+
+const RequestHelper = require('../helpers/request_helper.js');
+const PubSub = require('../helpers/pub_sub.js');
+
+
+const Countries = function () {
+  this.countries = [];
+};
+
+Countries.prototype.getCountriesAPIData = function () {
+  const requestHelper = new RequestHelper('https://restcountries.eu/rest/v2/all');
+  requestHelper.get((data) => {
+    this.filterData(data);
+    PubSub.publish('Countries:API-data-ready', this.countries);
+  });
+};
+
+Countries.prototype.filterData = function (dummyData) {
+ 
+};
+
+
+
+
+module.exports = Countries
