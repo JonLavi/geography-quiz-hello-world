88eeae8994a2d55880758f6d2c5ee3cafd8e4d28
Jonathan Lavi <jonathan.lavi@gmail.com>
2019-01-25 14:59:48 +0000

    write combineData() function

--- client/src/models/countries_collection.js
+++ client/src/models/countries_collection.js
@@ -5,3 +5,19 @@
 
 // Path 2
 // Use dummy data to request to request all of those countries API objects
+
+
+
+
+ContiresCollection.prototype.combineData = function (){
+  const countryHellos = this.hellos;
+  let countryApiData = this.countries;
+  countryApiData.forEach((apiCountry) => {
+    countryHellos.forEach((helloCountry) =>{
+      if (apiCountry.name === helloCountry.name){
+        apiCountry.hello = helloCountry.hello;
+      }
+    })
+  })
+  return countryApiData;
+};
