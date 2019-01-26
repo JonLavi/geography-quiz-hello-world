7765c67375e451797e8f5e97b9856b1008955721
Jonathan Lavi <jonathan.lavi@gmail.com>
2019-01-26 09:08:58 +0000

    make function to shuffle countries

--- client/src/models/game.js
+++ client/src/models/game.js
@@ -1,9 +1,42 @@
 const PubSub = require('../helpers/pub_sub.js');
 
 const Game = function () {
-
+  this.countries_data = []
 };
 
+Game.prototype.bindEvents() = function () {
+  PubSub.subscribe('Countries:game-data', (evt) => {
+    this.countries_data = evt;
+
+    this.prepareGame();
+
+
+  })
+};
+
+
+Game.prototype.prepareGame = function () {
+  const countrySelection = this.shuffleCountries(this.countries_data);
+};
+
+
+Game.prototype.shuffleCountries = function(countriesArray) {
+  let currentIndex = countriesArray.length;
+  let temporaryValue;
+  let randomIndex;
+
+  while (0 !== currentIndex) {
+
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    temporaryValue = countriesArray[currentIndex];
+    countriesArray[currentIndex] = countriesArray[randomIndex];
+    countriesArray[randomIndex] = temporaryValue;
+  }
+
+  return countriesArray;
+}
 
 
 module.exports = Game;
