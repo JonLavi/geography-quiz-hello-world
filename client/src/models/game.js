a945703c61310d4d1a4e162f718da33673f04ae7
Jonathan Lavi <jonathan.lavi@gmail.com>
2019-01-26 10:28:03 +0000

    add startGame() and playRound()

--- client/src/models/game.js
+++ client/src/models/game.js
@@ -8,14 +8,13 @@ const Game = function () {
 
 Game.prototype.bindEvents() = function () {
   PubSub.subscribe('Countries:game-data', (evt) => {
-    this.countries_data = evt;
+    this.countries_data = evt.detail;
 
     this.prepareGame();
 
-
+    this.startGame();
   })
 };
-
 
 Game.prototype.prepareGame = function () {
   const countrySelection = this.shuffleCountries(this.countries_data);
@@ -41,6 +40,38 @@ Game.prototype.shuffleCountries = function(countriesArray) {
   return countriesArray;
 }
 
+
+Game.prototype.startGame = function () {
+  let roundNumber = 0
+
+  while roundNumber < this.numberOfRounds {
+    this.playRound();
+  }
+
+  console.log('Game over!');
+};
+
+
+Game.prototype.playRound = function () {
+  let currentQuestion = this.selectionForGame[roundNumber];
+  let questionData = {
+    "name": currentQuestion.name,
+    "capital": currentQuestion.capital
+  }
+  PubSub.publish('Game:question-data-ready', questionData);
+
+  PubSub.subscribe('SkipView:skip-button-pressed', () => {
+    roundNumber ++;
+    // end round, increase round number, start new round
+  });
+
+  PubSub.subscribe('InputView:answer-submitted', (evt) => {
+    result = this.evaluateAnswer(evt.detail);
+    PubSub.publish('ResultView:result-ready', result);
+    roundNumber ++;
+    // end round
+  });
+};
 
 
 
