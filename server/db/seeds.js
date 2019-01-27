cd6f09eacd36d8aeff567cc6baecff0215d0ab51
Jonathan Lavi <jonathan.lavi@gmail.com>
2019-01-27 12:28:36 +0000

    add countries to seed file

---
+++ server/db/seeds.js
@@ -0,0 +1,49 @@
+use hello_world;
+db.dropDatabase();
+
+db.hellos.insertMany([
+  {name: "France", hello: "bonjour"},
+  {name: "Spain", hello: "hola"},
+  {name: "Germany", hello: "guten tag"},
+  {name: "Netherlands", hello: "Hallo"},
+  
+  {name: "Ukraine", hello: "Dobriy den"},
+  {name: "Protugal", hello: "Olá"},
+  {name: "Denmark", hello: "Hej"},
+  {name: "Estonia", hello: "Tere"},
+  {name: "Italy", hello: "Ciao"},
+  {name: "Slovakia", hello: "Ahoj"},
+
+  {name: "Bulgaria", hello: "Zdraveĭte"},
+  {name: "Iceland", hello: "Góðan dag"},
+  {name: "Hungaria", hello: "Jó napot"},
+  {name: "Greece", hello: "Geia sas}",
+  {name: "Lithuania", hello: "Sveiki"},
+  {name: "Croatia", hello: "Bok"},
+
+
+  {name: "Macedonia", hello: "Zdravo"},
+  {name: "Bosnia", hello: "Dobar dan"},
+  {name: "Belarus", hello: "Zdravstvujtie"},
+  {name: "Finnland", hello: "Hei"},
+  {name: "Norway", hello: "Hallo"},
+  {name: "Sweden", hello: "Hallå"},
+  {name: "Poland", hello: "Cześć"},
+  {name: "Malta ", hello:"Bonjour"},
+  {name: "Romania", hello: "Salut"},
+  {name: "Serbia", hello: "Zdravo"},
+  {name: "Slovenia", hello: "Živjo"},
+  {name: "Georgia", hello: "Komentari"},
+  {name: "Welsh", hello: "Helo"},
+  {name: "Albania ", hello:P"ërshëndetje"},
+  {name: "Armenia", hello: "Barev Dzez"},
+
+  {name: "Frisian", hello: "Goeie dei"},
+
+  {name: "Turkey", hello: "Merhaba"},
+  {name: "Russia", hello: "Zdravstvuyte"},
+  {name: "The Netherlands", hello: "Hallo"},
+  {name: "Luxembourg", hello: "Moïen"},
+  {name: "Basque", hello: "Kaixo"},
+  {name: "Czech Republic", hello: "Ahoj"},
+]);
