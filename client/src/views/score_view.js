cdd8876aff2676be0d6b279860c48d556e2b283d
Jonathan Lavi <jonathan.lavi@gmail.com>
2019-01-28 14:38:48 +0000

    subscribe to game over channel

--- client/src/views/score_view.js
+++ client/src/views/score_view.js
@@ -4,6 +4,13 @@ const ScoreView = function (container) {
   this.container = container
 }
 
+ScoreView.prototype.bindEvents = function () {
+  PubSub.subscribe('Game:score-given', (evt) => {
+    this.renderScore(evt.detail);
+  })
+};
+
+
 
 
 module.exports = ScoreView;
