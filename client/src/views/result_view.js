8a731654903e3d8dc5c1a31773798dfc66082f3b
Jonathan Lavi <jonathan.lavi@gmail.com>
2019-01-28 12:00:54 +0000

    add bind events

--- client/src/views/result_view.js
+++ client/src/views/result_view.js
@@ -4,5 +4,12 @@ const ResultView = function (container) {
   this.container = container;
 }
 
+ResultView.prototype.bindEvents = function () {
+  PubSub.subscribe('Game:result-ready', (evt) => {
+    this.renderResult(evt.detail);
+  });
+};
+
+
 
 module.exports = ResultView;
