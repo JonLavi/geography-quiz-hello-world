112c3e18860471eaa29ae36044130238ced014f0
Jonathan Lavi <jonathan.lavi@gmail.com>
2019-01-28 15:54:39 +0000

    answer checks the current question

--- client/src/models/game.js
+++ client/src/models/game.js
@@ -6,32 +6,34 @@ const Game = function () {
   this.numberOfRounds = 10
   this.score = 0
   this.questionAnswered = false
+  this.currentQuestion = {}
 };
 
 Game.prototype.bindEvents = function () {
   PubSub.subscribe('Countries:game-data', (evt) => {
-    this.countries_data = evt.detail;
 
-    // this.countries_data = dummyGameData // substitude data from server with dummy
-    const questionsForGame = this.prepareGame(this.countries_data, this.numberOfRounds);
-    // debugger
-    const currentQuestionData = this.makeNewQuestion(questionsForGame);
-    PubSub.publish('Game:question-data-ready', currentQuestionData);
+    // receive country data and make question pool
+    this.countries_data = evt.detail;
+    const questionPool = this.prepareGame(this.countries_data, this.numberOfRounds);
+
+    // make first question
+    this.currentQuestion = this.makeNewQuestion(questionPool);
+    PubSub.publish('Game:question-data-ready', this.currentQuestion);
+
 
     PubSub.subscribe('AnswerView:answer-submitted', (evt) => {
-      // debugger
       if (!this.questionAnswered){
-        result = this.evaluateAnswer(currentQuestionData, evt.detail);
+        result = this.evaluateAnswer(this.currentQuestion, evt.detail);
         PubSub.publish('Game:result-ready', result);
 
         this.score = this.givePoints(result, this.score);
         console.log('score:', this.score)
         PubSub.publish('Game:score-given', this.score);
-        this.questionAnswered = true
+        this.questionAnswered = true;
+
       } else {
         console.log('You have already answered this question!')
       }
-
 
       this.givePoints(result, this.score);
       PubSub.publish('Game:score-given', this.score);
@@ -40,8 +42,8 @@ Game.prototype.bindEvents = function () {
     });
 
     PubSub.subscribe('NextQuestionView:button-pressed', (evt) => {
-      const currentQuestionData = this.makeNewQuestion(questionsForGame);
-      PubSub.publish('Game:question-data-ready', currentQuestionData);
+      this.currentQuestion = this.makeNewQuestion(questionPool);
+      PubSub.publish('Game:question-data-ready', this.currentQuestion);
     });
 
   })
@@ -50,7 +52,6 @@ Game.prototype.bindEvents = function () {
 
 Game.prototype.makeNewQuestion = function (questionPool) {
   this.questionAnswered = false;
-  let currentQuestionData
   let currentQuestion = {};
 
   if (questionPool.length === 0) {
@@ -70,8 +71,8 @@ Game.prototype.makeNewQuestion = function (questionPool) {
 
 /////// Check answer workflow ///////
 
-Game.prototype.evaluateAnswer = function (currentQuestionData, answer) {
-  if (currentQuestionData.name.toLowerCase() === answer.toLowerCase()){
+Game.prototype.evaluateAnswer = function (question, answer) {
+  if (question.name.toLowerCase() === answer.toLowerCase()){
     return true;
   } else {
     return false;
