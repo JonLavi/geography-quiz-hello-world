77afdea0af56c9af910ac3f06ced8b96f527cbd4
Jonathan Lavi <jonathan.lavi@gmail.com>
2019-01-29 12:06:07 +0000

    render skip and next buttons differently

--- client/src/app.js
+++ client/src/app.js
@@ -39,7 +39,7 @@ const skipbutton = document.querySelector('button#skip')
 
 
   // viewer.initialiseWebGL()
-  viewer.TestBindAnswerEvents()
+  // viewer.TestBindAnswerEvents()
   // viewer.exlporationMode()
 
 
