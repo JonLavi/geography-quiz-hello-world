8f4690f9e24f138b70a5bb67b18c2cb9a1d4971b
Rick Symington <rick.symington@gmail.com>
2019-01-29 12:19:44 +0000

    Add startover to game over screen

--- client/src/views/game_over.js
+++ client/src/views/game_over.js
@@ -21,6 +21,14 @@ GameOverView.prototype.renderGameOver = function (score) {
     const resultParagraph = document.createElement('h2');
     resultParagraph.textContent = `Game Over. Your score was ${score}`
     gameOver.appendChild(resultParagraph)
+
+    const button = document.createElement('button')
+    button.textContent = 'Start Again'
+    button.addEventListener('click', function() {
+      document.location.reload(true)
+    })
+    gameOver.appendChild(button)
+
 }
 
 
