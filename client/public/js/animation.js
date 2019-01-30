0cd646f6f17a2790328bea3ce5e41bc7a5244125
Katharina01099 <yes>
2019-01-30 15:58:31 +0000

    title animation

---
+++ client/public/js/animation.js
@@ -0,0 +1,29 @@
+// Wrap every letter in a span
+
+document.addEventListener('DOMContentLoaded', () => {
+  const lettersSpan = document.querySelector('span#titleLetters')
+  letters = ['H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!']
+  letters.forEach( (letter) => {
+    const letterSpan = document.createElement('span')
+    letterSpan.classList.add('letter');
+    letterSpan.textContent = letter;
+    lettersSpan.appendChild(letterSpan);
+  });
+
+  anime.timeline({loop: true})
+  .add({
+    targets: '.grid-item .ml6 .letter',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: function(el, i) {
+      return 50 * i;
+  }
+  }).add({
+    targets: '.ml6',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+});
