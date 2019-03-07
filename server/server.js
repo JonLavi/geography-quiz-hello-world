aa2741bed4518557cd8a82e8229b3cd4c003097a
Jonathan Lavi <jonathan.lavi@gmail.com>
2019-03-07 11:31:10 +0000

    add process.env.MONGODB_URI to mongodb_driver

--- server/server.js
+++ server/server.js
@@ -3,9 +3,10 @@ const app = express();
 const path = require('path');
 const parser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
-const createRouter = require('./helpers/create_router.js')
-const fetch = require('node-fetch');
-const mdb = require('./helpers/mongodb_connection.js');
+const createRouter = require('./helpers/create_router.js');
+const mongodb_connection = require('./helpers/mongodb_connection.js');
+// const fetch = require('node-fetch');
+
 
 const publicPath = path.join(__dirname, '../client/public');
 app.use(express.static(publicPath));
@@ -16,7 +17,7 @@ app.get('/', function (req, res) {
   res.sendFile('index.html');
 });
 
-MongoClient.connect(mdb, { useNewUrlParser: true })
+MongoClient.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
   .then((client) => {
     const db = client.db('heroku_3mr9jt7k');
     const hellosCollection = db.collection('hellos');
